db3f1184be4c1bbe794dd98839dcc1fae104a8f5
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-07 04:12:43 +0900

    オウム返しBotのチュートリアルを実践

--- api.py
+++ api.py
@@ -1,7 +1,56 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
+app = Flask(__name__)
+
 
 load_dotenv()
 
 CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
+CHANNEL_SECRET = os.environ['CHANNEL_SECRET']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+# # test endpoint
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>'
+
+
+# endpoint from linebot
+@app.route('/callback', methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print('Invalid signature. Please check your channel access token/channel secret.')
+        abort(400)
+
+    return 'OK'
+
+
+# handle message from LINE
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+if __name__ == '__main__':
+    app.run()
