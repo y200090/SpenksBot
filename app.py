bfcd4f82667618904329e6edc61b4f06c7db873f
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-08 02:54:39 +0900

    試行錯誤1回目

--- app.py
+++ app.py
@@ -47,9 +47,11 @@ def callback():
 # handle message from LINE
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    messages = [TextSendMessage(text=f'{event.message.text}と入力されました。'), TextSendMessage(text='challenge success')]
+    
     line_bot_api.reply_message(
         event.reply_token,
-        [TextSendMessage(text=f'{event.message.text}と入力されました。'), TextSendMessage(text='challenge success')]
+        messages
     )
 
 
