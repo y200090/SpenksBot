cb3e66d2cf4338d85b944f1c8cbcdd34edde90e3
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-08 02:53:03 +0900

    再チャレンジ

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=[f'{event.message.text}と入力されました。', 'challenge success'])
+        [TextSendMessage(text=f'{event.message.text}と入力されました。'), TextSendMessage(text='challenge success')]
     )
 
 
