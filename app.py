dced18927ce520159daf5304ce107dca095bebc1
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-08 02:50:54 +0900

    linebot複数メッセージ対応

--- app.py
+++ app.py
@@ -49,11 +49,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=f'{event.message.text}と入力されました。')
-    )
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=[f'{event.message.text}と入力されました。', 'challenge success'])
     )
 
 
